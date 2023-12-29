706cae4a871140ccc36aec5353a23a447cca787e
emmanuel <Idokoemmanuel3@gmail.com>
2023-12-29 19:57:57 +0100

    updated user model class to implement mustverifyemail interface

--- laravel-api-1/app/Providers/FortifyServiceProvider.php
+++ laravel-api-1/app/Providers/FortifyServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Contracts\{LoginResponse, RegisterResponse};
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -20,7 +22,32 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
+            public function toResponse($request)
+            {
+                if($request->wantsJson()) {
+                    $user = User::where('email', $request->email)->first();
+                    return response()->json([
+                        "message" => "You are successfully logged in",
+                        "token" => $user->createToken($request->email)->plainTextToken,
+                    ], 200);
+                }
+                return redirect()->intended(Fortify::redirects('login'));
+            }
+        });
+     
+        $this->app->instance(RegisterResponse::class, new class implements RegisterResponse {
+            public function toResponse($request)
+            {
+                $user = User::where('email', $request->email)->first();
+                return $request->wantsJson()
+                    ? response()->json([
+                        'message' => 'Registration successful, verify your email address',
+                        "token" => $user->createToken($request->email)->plainTextToken,
+                        ], 200)
+                    : redirect()->intended(Fortify::redirects('register'));
+            }
+        });
     }
 
     /**
