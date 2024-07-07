308de1756e3da5fd9d3f144849c31fcb889ab95c
Idoko Emmanuel <idokoemmanuel3@gmail.com>
2024-07-07 01:31:39 +0100

    feat: add logout to welcome blade

--- laravel-api-3/resources/views/welcome.blade.php
+++ laravel-api-3/resources/views/welcome.blade.php
@@ -12,6 +12,14 @@
             <div class="p-6 text-right sm:fixed sm:top-0 sm:right-0">
                 @auth
                     <a href="{{ route('home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Home</a>
+                    <div class="inline-block">
+                        <form method="POST" action="{{ route('logout') }}" class="inline">
+                            @csrf
+                            <button type="submit" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">
+                                Logout
+                            </button>
+                        </form>
+                    </div>
                 @else
                     <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Log in</a>
 
